611ab520ede18751d43cdeee51ce9918546d16dc
Tas <tasbcc170@gmail.com>
2024-11-07 08:30:50 +0000

    commit

--- calculator.py
+++ calculator.py
@@ -3,23 +3,44 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
-        return result
+        if b > 0:
+            for i in range(b):
+                result = self.add(result, a)
+            return result
+        else:
+            for i in range(a):
+                result = self.add(result, b)
+            return result
+    
+
+        
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+        c = False
+        if b == 0:
+            return "undefined"
+        
+        if a < 0 or b < 0:
+            c = True
+            a = abs(a)
+            b = abs(b)
+
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
+        
+        if c ==  True:
+            result = -result
         return result
+            
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
@@ -29,6 +50,6 @@ if __name__ == "__main__":
     print("This is a simple calculator class!")
     print("Example: addition: ", calc.add(1, 2))
     print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
+    print("Example: multiplication: ", calc.multiply(2, -3))
+    print("Example: division: ", calc.divide(10,10))
     print("Example: modulo: ", calc.modulo(10, 3))
--- test_calculator.py
+++ test_calculator.py
@@ -11,5 +11,36 @@ class TestCalculator(unittest.TestCase):
 
     # Add the following test methods to the TestCalculator class:
 
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(50,6),56)
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(10,20),30)
+
+    def test_sub(self):
+        self.assertEqual(self.calc.subtract(-10,-2),-12)
+
+    def test_sub(self):
+        self.assertEqual(self.calc.subtract(3,50),-47)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(-5,2),-10)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(10,-10),-100)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10,10),1)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10,0),"undefined")
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10,2),0)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(2,10),2)
+
 if __name__ == '__main__':
     unittest.main()
